950868bf0df03594186ea01c4a4a1f87076bdb50
steackfrite <pro@gabriel-corre.fr>
2021-11-28 17:12:34 +0100

    Add echo function

--- meteo_france_bot.py
+++ meteo_france_bot.py
@@ -5,11 +5,17 @@ from os import getenv
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Let's rock 'n roll!")
 
+def echo(update: Update, context: CallbackContext):
+    message_01 = 'Envoyez-moi une commande et non pas un message !' # souligner commande
+    message_02 = 'Taper /help pour voir la liste des commandes possibles' # souligner /help
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message_01)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message_02)
 
 def main():
     # Variables
@@ -26,10 +32,12 @@ def main():
 
     ## Listening to commands ##
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     ## Listening to commands ##
 
     ## Add command to the dispatcher ##
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(echo_handler)
     ## Add command to the dispatcher ##
 
     # Launch the bot!
